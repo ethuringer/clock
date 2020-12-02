7a3ea437322d10545bcdceb22137a5b8fbf481b4
ethuringer <ethuringer@gmail.com>
2020-12-02 14:42:19 +0100

    correction of timer

--- js/clock.js
+++ js/clock.js
@@ -1,6 +1,10 @@
-const now = (date) => new Intl.DateTimeFormat([], {
-    timeStyle: 'medium',
-    hour12: false
-}).format(new Date());
+function realTime() {
+    const now = new Date();
+    const displayedTime = new Intl.DateTimeFormat([], {
+        timeStyle: 'medium',
+        hour12: false
+    }).format(now);
 
-document.querySelector('.clock').textContent = now(new Date());
+    document.querySelector('.clock').textContent = displayedTime;
+}
+let showSeconds = setInterval(realTime, 0, 1000);
